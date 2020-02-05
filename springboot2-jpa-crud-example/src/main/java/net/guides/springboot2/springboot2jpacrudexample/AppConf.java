cd3a2ad115d27de9e3678cfb00a2323ed8f7b3ae
sandesh <qalamsal8@gmail.com>
2020-02-05 11:22:39 -0600

    hibernate core dep added

--- springboot2-jpa-crud-example/src/main/java/net/guides/springboot2/springboot2jpacrudexample/AppConf.java
+++ springboot2-jpa-crud-example/src/main/java/net/guides/springboot2/springboot2jpacrudexample/AppConf.java
@@ -10,7 +10,7 @@ public class AppConf implements WebMvcConfigurer {
 	 @Override
 	    public void addCorsMappings(CorsRegistry registry) {
 	        registry.addMapping("/**")
-	                .allowedOrigins("http://localhost:3000","http://localhost:4200")
+	                .allowedOrigins("http://localhost:3000")
 	                .allowedMethods("*"); //every methods are thing passing..
 	    }
 }
